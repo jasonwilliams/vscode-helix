d00243928740ebe2011b39e332dbdc5d36f2cf76
jasonwilliams <jase.williams@gmail.com>
2024-01-23 22:38:41 +0000

    Support VS Code For Web

--- build.mjs
+++ build.mjs
@@ -1,40 +1,37 @@
-const production = process.argv[2] === '--production';
 import esbuild from 'esbuild';
-
+const production = process.argv[2] === '--production';
 const watch = process.argv[2] === '--watch';
-const context = await esbuild
-  .context({
-    entryPoints: ['./src/index.ts'],
-    bundle: true,
-    outdir: 'dist',
-    external: ['vscode'],
-    format: 'cjs',
-    sourcemap: !production,
-    minify: production,
-    platform: 'node',
-    target: 'ES2022',
-    plugins: [
-      {
-        name: 'watch',
-        setup(build) {
-          build.onEnd(() => {
-            if (watch) console.log('build finished');
-          });
-          build.onStart(() => {
-            if (watch) console.log('building');
-          });
-        },
-      },
-    ],
-  })
-  .catch((e) => {
-    console.error(e);
-    process.exit(1);
-  });
+let desktopContext, browserContext;
+
+// This is the base config that will be used by both web and desktop versions of the extension
+const baseConfig = {
+  entryPoints: ['./src/index.ts'],
+  bundle: true,
+  external: ['vscode'],
+  sourcemap: !production,
+  minify: production,
+  target: 'ES2022',
+  format: 'cjs',
+};
+
+try {
+  [desktopContext, browserContext] = await Promise.all([
+    // https://esbuild.github.io/getting-started/#bundling-for-node
+    esbuild.context({ ...baseConfig, outfile: './dist/index.js', platform: 'node' }),
+    // https://esbuild.github.io/getting-started/#bundling-for-the-browser
+    esbuild.context({ ...baseConfig, outfile: './dist/browser.js', platform: 'browser' }),
+  ]);
+} catch (e) {
+  console.error(e);
+  process.exit(1);
+}
 
 if (watch) {
-  await context.watch();
+  await desktopContext.watch();
+  await browserContext.watch();
 } else {
-  context.rebuild();
-  context.dispose();
+  desktopContext.rebuild();
+  browserContext.rebuild();
+  desktopContext.dispose();
+  browserContext.dispose();
 }
